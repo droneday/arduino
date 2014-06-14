70f4bac6b063b3001b0f1e6da0fe025fb9a5b256
Simon Howard <fraggle@gmail.com>
2014-06-14 02:31:31 -0400

    Add Bluetooth comms code.
    This is just some initial basic communications code and is not
    yet hooked in with the main code. I also haven't been able to fully
    test it with a Bluetooth module due to some hardware issues. However,
    based on the testing I have been able to perform I'm reasonably
    confident that this mostly ought to work.

--- DroneDay.ino
+++ DroneDay.ino
@@ -1,3 +1,5 @@
+#include <SoftwareSerial.h>
+
 #include "I2Cdev.h"
 
 #include "MPU6050.h" // not necessary if using MotionApps include file
@@ -8,6 +10,24 @@
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
+
+// Friendly name to use for Bluetooth ID:
+#define BLUETOOTH_DRONE_NAME "DroneDayDrone"
+
+// Pin numbers for I/O pins connected to the Bluetooth board.
+// TODO: Configure these correctly.
+int bluetoothKey = -1;
+int bluetoothTx = 5;
+int bluetoothRx = 6;
+
+// Software UART for communicating with the Bluetooth board.
+SoftwareSerial bluetooth(bluetoothRx, bluetoothTx);
+
+// Input buffer for reading from the Bluetooth board.
+String bluetoothBuffer = "";
+
+// Current movement state received over Bluetooth.
+int moveX, moveY, moveZ, yawTurn;
 
 MPU6050 mpu;
 
@@ -38,6 +58,9 @@ void setup() {
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
   
     calibrate_imu();
+
+    // TODO:
+    //bluetoothSetup();
 /*
 float x,y,z;
     quat2rpy(rpy2quat(0.1, .2, .3), &x, &y, &z);
@@ -118,6 +141,9 @@ void loop() {
     // blink LED to indicate activity
     blinkState = !blinkState;
     digitalWrite(LED_PIN, blinkState);
+
+    // TODO:
+    //bluetoothPoll();
 }
 
 Quaternion rpy2quat(float x, float y, float z)
@@ -201,4 +227,128 @@ void readIMU()
   roll = roll*(1-accelFrac) + accel_roll*accelFrac;
   
   printrpy(roll, pitch, yaw);
+}
+
+// Setup function to initialize Bluetooth receiver.
+void bluetoothSetup() {
+
+  if (bluetoothKey >= 0) {
+    // Bluetooth board starts in passthrough mode.
+    pinMode(bluetoothKey, OUTPUT);
+    digitalWrite(bluetoothKey, LOW);
+  }
+
+  // Set up serial.
+  bluetooth.begin(9600);
+  bluetooth.println("hello world");
+
+  // Default drone name,
+  bluetoothATCommand("AT+NAME=" BLUETOOTH_DRONE_NAME);
+}
+
+// Read a line of text from the Bluetooth device, stripping
+// the newline character.
+String bluetoothReadLine() {
+  String result = "";
+  for (;;) {
+    if (bluetooth.available()) {
+      char c = bluetooth.read();
+      if (c == '\r' || c == '\n') {
+        break;
+      } else {
+        result += c;
+      }
+    }
+  }
+  return result;
+}
+
+// Execute an AT command on the Bluetooth board.
+String bluetoothATCommand(String command) {
+
+  if (bluetoothKey < 0) {
+    return "Not supported.";
+  }
+
+  // Switch into AT command mode by toggling the KEY pin high.
+  digitalWrite(bluetoothKey, HIGH);
+  delay(250);  // TODO: Check if delay is needed.
+
+  // Send command to board and read the response.
+  bluetooth.println(command);
+  String response = bluetoothReadLine();
+
+  // Toggle back into normal passthrough mode.
+  digitalWrite(bluetoothKey, LOW);
+  delay(250);  // TODO: Check if delay is needed.
+
+  return response;
+}
+
+// Dump current configured control state over Bluetooth channel.
+void bluetoothDumpState() {
+  bluetooth.print("X: ");
+  bluetooth.print(moveX);
+  bluetooth.print(" Y: ");
+  bluetooth.print(moveY);
+  bluetooth.print(" Z: ");
+  bluetooth.print(moveZ);
+  bluetooth.print(" T: ");
+  bluetooth.print(yawTurn);
+  bluetooth.println();
+}
+
+// Parse and execute a complete command read from the Bluetooth
+// module.
+void bluetoothCommand() {
+  if (bluetoothBuffer.length() < 1) {
+    return;
+  }
+  switch (toupper(bluetoothBuffer[0])) {
+
+    // Normal flight commands: Adjust position and turn on
+    // axis.
+    case 'X':
+      moveX = bluetoothBuffer.substring(1).toInt();
+      break;
+    case 'Y':
+      moveY = bluetoothBuffer.substring(1).toInt();
+      break;
+    case 'Z':
+      moveZ = bluetoothBuffer.substring(1).toInt();
+      break;
+    case 'T':
+      yawTurn = bluetoothBuffer.substring(1).toInt();
+      break;
+
+    // For debugging.
+    case 'D':
+      bluetoothDumpState();
+      break;
+
+    // Send AT command to Bluetooth board and read back the
+    // response. Useful if we want to do something like change
+    // the Bluetooth display name.
+    case '#':
+      bluetooth.println(bluetoothATCommand(bluetoothBuffer.substring(1)));
+      break;
+
+    default:
+      bluetooth.println("Unknown command type!");
+      break;
+  }
+}
+
+void bluetoothPoll() {
+  // Check for new characters received over Bluetooth.
+  // When a complete line has been read, process it as a command.
+  while (bluetooth.available()) {
+    char c = bluetooth.read();
+    if (c == '\r' || c == '\n') {
+      bluetoothCommand();
+      bluetoothBuffer = "";
+    } else {
+      bluetoothBuffer += c;
+    }
+  }
 }
